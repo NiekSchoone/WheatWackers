78686a653a193fd7e6afa39221cb1c70c67365c2
Collix <15986@ma-web.nl>
2017-03-13 16:10:29 +0100

    + Movetowards() function for player movement and movable-tile checking
    (+ Made a tempoary grid system for testing purposes.)

---
+++ Game_TS/src/Player/TempGrid.ts
@@ -0,0 +1,37 @@
+﻿class TempGrid extends Phaser.Sprite
+{
+
+    rows: Array<Array<Phaser.Sprite>>;
+
+    constructor(game: Phaser.Game)
+    {
+        super(game, 0, 0);
+        this.anchor.setTo(0.5);
+        this.scale.set(0.25);
+        this.createGrid();
+    }
+
+    private createGrid()
+    {
+        this.rows = new Array<Array<Phaser.Sprite>>();
+
+        for (var i = 0; i < 3; i++)
+        {
+            var row: Array<Phaser.Sprite> = new Array<Phaser.Sprite>();
+
+            for (var k = 0; k < 3; k++)
+            {
+                var tile = new TempTile(this.game);
+               
+                tile.x =    tile.width * k;
+                tile.y = tile.height * i;
+
+                this.addChild(tile);
+
+                row.push(tile);
+            }
+            
+            this.rows.push(row);
+        }
+    }
+}
---
+++ Game_TS/src/Player/TempTile.ts
@@ -0,0 +1,17 @@
+﻿class TempTile extends Phaser.Sprite
+{
+    game : Phaser.Game
+    constructor(game: Phaser.Game)
+    {
+        super(game, 0, 0);
+        this.game = game;
+        this.createImage();
+    }
+
+    createImage()
+    {
+        var image = this.addChild(this.game.make.sprite(0, 0, "grass"));
+        this.anchor.set(0.5, 0.5);
+        this.resizeFrame(this, this.game.cache.getImage("grass").width, this.game.cache.getImage("grass").height);
+    }
+}
