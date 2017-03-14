9b3d3aad01382ab0d2549b5b150c7918cec45d89
NiekSchoone <niek.schoone@quicknet.nl>
2017-03-14 09:01:28 +0100

    Small fix in grid, ignore app.ts/.js

--- Game_TS/src/Grid/Grid.ts
+++ Game_TS/src/Grid/Grid.ts
@@ -4,26 +4,27 @@
     gridHeight: number;
     Tiles: Tile[][];
     space: Phaser.Key;
+
     constructor(_game: Phaser.Game) {
         this.game = _game;
     }
+
     public generateGrid(_gridWidth: number, _gridHeight: number) {
-        
         this.gridWidth = _gridWidth;
         this.gridHeight = _gridHeight;
         this.Tiles = [];
 
         for (let x = 0; x < this.gridWidth; x++){
-            this.Tiles[x]= []
+            this.Tiles[x] = [];
             for (let y = 0; y < this.gridHeight; y++) {
                 let newTile: Tile = new Tile(this.game, x, y);
-                newTile.setTile(true);
+                //newTile.setTile(true);
                 this.Tiles[x][y] = newTile;
             }
         }
-    
     }
-    public getTile(_x: number, _y: number, has) {
+
+    public getTile(_x: number, _y: number) {
         return this.Tiles[_x][_y];
     }
 }
