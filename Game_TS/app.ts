e725f5539a3b0ba1a73ed0e775b143c6fc43f07f
Collix <15986@ma-web.nl>
2017-03-15 20:19:58 +0100

    Working on player wheat cutting

--- Game_TS/app.ts
+++ Game_TS/app.ts
@@ -10,7 +10,7 @@ class Game {
     private background: Phaser.TileSprite;
 
     constructor() {
-        this.game = new Phaser.Game(document.body.clientWidth, document.body.clientHeight, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(1200, 800, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
 
         /*SOCKET.on("player_joined", function (data) {
             console.log(data + " has joined");
@@ -18,19 +18,19 @@ class Game {
     }
 
     preload() {
-        this.game.load.image('background', 'Images/Level/background.jpg');
-        this.game.load.image('wheat_1', 'Images/Level/wheat_01.png');
-        this.game.load.image('wheat_2', 'Images/Level/wheat_02.png');
-        this.game.load.image('wheat_3', 'Images/Level/wheat_03.png');
-        this.game.load.image('wheat_4', 'Images/Level/wheat_04.png');
-        this.game.load.image('wheat_5', 'Images/Level/wheat_05.png');
-        this.game.load.image('wheat_cut_1', 'Images/Level/wheat_cut_01.png');
-        this.game.load.image('wheat_cut_2', 'Images/Level/wheat_cut_02.png');
-        this.game.load.image('wheat_cut_3', 'Images/Level/wheat_cut_03.png');
-        this.game.load.image('obstacle_1', 'Images/Level/obstacle_01.png');
-        this.game.load.image('obstacle_2', 'Images/Level/obstacle_02.png');
-        this.game.load.image('obstacle_3', 'Images/Level/obstacle_03.png');
-        this.game.load.image('button_join', 'Images/Level/button_join.png');
+        this.game.load.image('background', 'assets/images/level/background.jpg');
+        this.game.load.image('wheat_1', 'assets/images/level/wheat_01.png');
+        this.game.load.image('wheat_2', 'assets/images/level/wheat_02.png');
+        this.game.load.image('wheat_3', 'assets/images/level/wheat_03.png');
+        this.game.load.image('wheat_4', 'assets/images/level/wheat_04.png');
+        this.game.load.image('wheat_5', 'assets/images/level/wheat_05.png');
+        this.game.load.image('wheat_cut_1', 'assets/images/level/wheat_cut_01.png');
+        this.game.load.image('wheat_cut_2', 'assets/images/level/wheat_cut_02.png');
+        this.game.load.image('wheat_cut_3', 'assets/images/level/wheat_cut_03.png');
+        this.game.load.image('obstacle_1', 'assets/images/level/obstacle_01.png');
+        this.game.load.image('obstacle_2', 'assets/images/level/obstacle_02.png');
+        this.game.load.image('obstacle_3', 'assets/images/level/obstacle_03.png');
+        this.game.load.image('button_join', 'assets/images/level/button_join.png');
     }
 
     create() {
@@ -40,9 +40,10 @@ class Game {
         this.game.add.existing(this.background);
 
         this.grid = new Grid(this.game);
-        this.grid.generateGrid(6, 6);
+        this.grid.generateGrid(4, 4);
 
-        this.player = new Player(this.game);
+        this.player = new Player(this.game, this.grid, "username");
+        this.game.add.existing(this.player);
     }
 }
 
--- Game_TS/src/Player/Player.ts
+++ Game_TS/src/Player/Player.ts
@@ -13,13 +13,12 @@
     constructor(game: Phaser.Game, grid : Grid, username : string)
     {
         super(game, 0, 0, "failguy");
-
+        console.log(TileState[1]);
         this.grid = grid;
         this.position.set(grid.getTile(0, 0).getX(), grid.getTile(0, 0).getY());
         this.anchor.setTo(0.5);
 
         this.moveDistance = this.grid.tilewidth;
-        console.log(this.moveDistance);
         this.scale.setTo(1);
         this.game = game;
 
@@ -36,33 +35,57 @@
 
     moveUpwards()
     {
-        this.moveTowards(this.x, this.y - this.moveDistance);
+        var tile = this.grid.getTileAtPlayer(this.x, this.y, 0, -1);
+
+        this.moveTowards(this.x, this.y - this.moveDistance, tile);
     }
 
     moveDownwards()
     {
-        this.moveTowards(this.x, this.y + this.moveDistance); 
+        var tile = this.grid.getTileAtPlayer(this.x, this.y, 0, 1);
+        
+        this.moveTowards(this.x, this.y + this.moveDistance, tile);
+         
     }
 
     moveLeft()
     {
-        this.moveTowards(this.x - this.moveDistance, this.y);
+        var tile = this.grid.getTileAtPlayer(this.x, this.y, -1, 0);
+
+        
+        this.moveTowards(this.x - this.moveDistance, this.y, tile);
+        
     }
 
     moveRight()
     {
-        if (this.grid.getTileAtPlayer(this.x, this.y, 1 ,0 ))
-        {
-               this.moveTowards(this.x + this.moveDistance, this.y);
-        }
-        
+        var tile = this.grid.getTileAtPlayer(this.x, this.y, 1, 0);
+    
+        this.moveTowards(this.x + this.moveDistance, this.y, tile);
     }
 
-    moveTowards(_x: number, _y: number)
+    moveTowards(_x: number, _y: number, tile: Tile)
     {
-        this.moving = true;
-        var tween: Phaser.Tween = this.game.add.tween(this.body).to({ x: _x - this.width / 2, y: _y - this.height / 2}, this.game.physics.arcade.distanceToXY(this, _x, _y) / this.speed * 1000, Phaser.Easing.Linear.None, true);
-        tween.onComplete.add(this.onComplete, this);
+        if (tile)
+        {
+            var tileState = tile.GetState();
+            
+            if (tileState == TileState.CUT || tileState == TileState.OBSTACLE)
+            {
+                this.moving = true;
+                var tween: Phaser.Tween = this.game.add.tween(this.body).to({ x: _x - this.width / 2, y: _y - this.height / 2 }, this.game.physics.arcade.distanceToXY(this, _x, _y) / this.speed * 1000, Phaser.Easing.Linear.None, true);
+                tween.onComplete.add(this.onComplete, this);
+            }
+            else if (true)
+            {
+
+            }
+        }
+    }
+
+    cutWheat()
+    {
+        
     }
 
     onComplete()
