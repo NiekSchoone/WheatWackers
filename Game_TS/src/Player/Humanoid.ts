6a3b0c55b6931cffd14f390f92f3073fd77a1ad2
Collix <15986@ma-web.nl>
2017-03-16 11:20:32 +0100

    Renamed opponent class to humanoid class, made movement for that.

--- Game_TS/src/Player/Humanoid.ts
+++ Game_TS/src/Player/Humanoid.ts
@@ -3,14 +3,13 @@
     speed: number = 1000;
     constructor(game : Phaser.Game)
     {
-        super(game, 0, 0);
+        super(game, 0, 0, "failguy");
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.physics.arcade.enable(this);
     }
 
     public moveTowards(x: number, y: number)
     {
-        
         var tween: Phaser.Tween = this.game.add.tween(this.body).to({ x: x - this.width / 2, y: y - this.height / 2 }, this.game.physics.arcade.distanceToXY(this, x, y) / this.speed * 1000, Phaser.Easing.Linear.None, true); 
     }
 }
